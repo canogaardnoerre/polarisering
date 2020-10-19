e07b64904a90f1a0f3f68b236075ffe8f598356f
annamariabukalska <56019427+annamariabukalska@users.noreply.github.com>
2020-10-19 21:07:16 +0200

    Fikser bilde og animasjon

--- app/js/app.js
+++ app/js/app.js
@@ -1,19 +1,22 @@
+function addAnimation() {
+  let text1 = document.querySelector('.title');
 
-var text1 = document.querySelector('.section-set1');
+  text1.innerHTML = text1.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+  
+  anime.timeline({loop: true})
+    .add({
+      targets: '.title .letter',
+      opacity: [0,1],
+      easing: "easeInOutQuad",
+      duration: 200,
+      delay: (el, i) => 150 * (i+1)
+    }).add({
+      targets: '.title',
+      opacity: 0,
+      duration: 750,
+      easing: "easeOutExpo",
+      delay: 0
+    });
+}
 
-text1.innerHTML = text1.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.text1 .letter',
-    opacity: [0,1],
-    easing: "easeInOutQuad",
-    duration: 2250,
-    delay: (el, i) => 150 * (i+1)
-  }).add({
-    targets: '.ml3',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
+addAnimation()
