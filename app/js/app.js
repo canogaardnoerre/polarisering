81e408028e567481f1aaf55556938a1c25a521b7
annamariabukalska <56019427+annamariabukalska@users.noreply.github.com>
2020-10-19 20:59:02 +0200

    WIP

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,19 @@
-"use strict";console.log("hello");
+
+var text1 = document.querySelector('.section-set1');
+
+text1.innerHTML = text1.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.text1 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.ml3',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
